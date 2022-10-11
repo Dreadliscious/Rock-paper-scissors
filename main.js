4e7ee4db2d68b11e1ba66e45ef0be89fbc434ad5
damien desparain <damien.cpielerins@gmail.com>
2022-10-11 10:32:10 +0200

    player choice

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 // creer une fonction qui renvoie un random rock paper scissors
-const choices = ['Rock', 'Paper', 'Scissors'];
 function getComputerChoice() {
-  var computerChoice = Math.floor(Math.random() * choices.length);
+  const choices = ['Rock', 'Paper', 'Scissors'];
+  var computerPlay = Math.floor(Math.random() * choices.length);
 
-  return choices[computerChoice];
+  return choices[computerPlay];
 };
 
-//console.log(getComputerChoice());
+console.log(getComputerChoice());
 // cette fonction est le choix du pc
 
 // demander au joueur de faire un choix
 // entrer ce choix dans une fonction choix du joueur
 // comparer la valeur de fonction pc et fonction joueur
+function play(playerSelection) {
+  let ComputerSelection = computerPlay();
+  let result ="";
+
+  if (playerSelection == computerSelection) {
+    result = 'You both harnessed' + playerSelection;
+  }
+  else if ((playerSelection == 'Rock' && computerSelection == 'scissors') ||
+  (playerSelection == 'paper' && computerSelection == 'rock') ||
+  (playerSelection == 'scissors' && computerSelection == 'paper')) {
+    result = "You win";
+  }
+  else {
+    result = "Defeat!";
+  }
+}
 // entrer le resultat dans une fonction resultat
 //renvoyer un message annoncant le resultat
