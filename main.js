1ce4c8440fb081bff62d3cad91499a4add88a14f
damien desparain <damien.cpielerins@gmail.com>
2022-10-11 13:49:38 +0200

    reset, 1st atempt wasnt right

--- main.js
+++ main.js
@@ -1,32 +1,7 @@
-// creer une fonction qui renvoie un random rock paper scissors
-function getComputerChoice() {
-  const choices = ['Rock', 'Paper', 'Scissors'];
-  var computerPlay = Math.floor(Math.random() * choices.length);
+// Prendre un integer random inclus entre 0 et 2 => choix du computer
 
-  return choices[computerPlay];
-};
+//convertir cet integer en string entre rock(0) paper (1) scissors (2)
 
-console.log(getComputerChoice());
-// cette fonction est le choix du pc
+//creer un message d erreur si l user fait un choix autre que rock paper scissors
 
-// demander au joueur de faire un choix
-// entrer ce choix dans une fonction choix du joueur
-// comparer la valeur de fonction pc et fonction joueur
-function play(playerSelection) {
-  let ComputerSelection = computerPlay();
-  let result ="";
-
-  if (playerSelection == computerSelection) {
-    result = 'You both harnessed' + playerSelection;
-  }
-  else if ((playerSelection == 'Rock' && computerSelection == 'scissors') ||
-  (playerSelection == 'paper' && computerSelection == 'rock') ||
-  (playerSelection == 'scissors' && computerSelection == 'paper')) {
-    result = "You win";
-  }
-  else {
-    result = "Defeat!";
-  }
-}
-// entrer le resultat dans une fonction resultat
-//renvoyer un message annoncant le resultat
+// prendre les 2 string player et computer et dire qui gagne
